ecd1831a9a7d32d4cf8cba34c3a155a82ba13a77
Bina Basiri <bina712010@gmail.com>
2024-12-24 14:30:06 -0500

    getting rid of chessknight icon

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,6 @@
 import { useState } from 'react';
 import { motion } from 'framer-motion';
-import {
-  ChevronRight,
-  CastleIcon as ChessKnight,
-  Dumbbell,
-  Brain,
-  Users,
-  Trophy,
-} from 'lucide-react';
+import { ChevronRight, Dumbbell, Brain, Users, Trophy } from 'lucide-react';
 import './Home.scss';
 import YouTube from 'react-youtube';
 
@@ -25,11 +18,7 @@ export default function ChesstaminaLanding() {
       title: 'Physical Fitness',
       description: 'Improve your physical health while playing',
     },
-    {
-      icon: <ChessKnight className="icon" />,
-      title: 'Strategic thinking',
-      description: 'Challenge yourself and test your strategies',
-    },
+
     {
       icon: <Users className="icon" />,
       title: 'Social Interaction',
